e438e677a315d368aa145f4b1fc0966b7490c2bf
alexadumi <alexandra.dumitru1218@gmail.com>
2018-11-09 01:18:19 +0200

    Comit initial
    Curs initial

---
+++ Curs2/src/Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge = 29;
+        System.out.print(myAge);
+        float weight = 60.5f;
+        double height = 356.6;
+        long distance = 10231220002331112l;
+        char gender = 'm';
+        boolean isStudent = false;
+        boolean hasJob = true;
+
+    }
+}
